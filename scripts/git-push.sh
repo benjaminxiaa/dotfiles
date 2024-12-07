cfab9df351001251055fcb0e49fd45dfa11b2c35
Benjamin Xia <xiaa.benjamin@gmail.com>
2024-12-06 23:12:10 -0800

    updated git push script

--- scripts/git-push.sh
+++ scripts/git-push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git add . && git commit -m "$1" && git push -u origin $(git symbolic-ref --short HEAD)
-
+if [ "$1" = "-m" ]; then
+    # Add all changes, commit with the message after -m, and push to current branch
+    git add -A && git commit -m "$2" && git push origin $(git symbolic-ref --short HEAD)
+fi

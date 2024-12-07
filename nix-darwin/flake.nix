fdbd10b7e9230defac794aa24f8dffe8b264d69b
Benjamin Xia <xiaa.benjamin@gmail.com>
2024-12-07 11:34:02 -0800

    added touch id support to nix flake file

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -94,6 +94,9 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Enable TouchId for sudo
+      security.pam.enableSudoTouchIdAuth = true;
     };
   in
   {
